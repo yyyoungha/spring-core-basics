824d6f57268c6886d3a4c40b0a46156d1a08ae83
yyyoungha <yyyoungha@gmail.com>
2023-02-04 17:54:53 +0900

    Add Order

---
+++ src/main/java/hello/demo/discount/DiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.demo.discount;
+
+import hello.demo.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+
+
+}
---
+++ src/test/java/hello/demo/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.demo.order;
+
+import hello.demo.OrderApp;
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
